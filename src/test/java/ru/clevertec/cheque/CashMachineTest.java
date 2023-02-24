b457b119804a880d4aa74af25e56c4d240dc7784
Dobrowydka <andrikchannelyou@gmail.com>
2023-02-24 21:19:02 +0300

    refactor: optimizing imports in cash machine test

--- src/test/java/ru/clevertec/cheque/CashMachineTest.java
+++ src/test/java/ru/clevertec/cheque/CashMachineTest.java
@@ -1,14 +1,10 @@
 package ru.clevertec.cheque;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -48,7 +44,7 @@ class CashMachineTest {
             put("discount", "2.5950");
         }};
 
-        List<Map<String, String>> list = new ArrayList<>(){{
+        List<Map<String, String>> list = new ArrayList<>() {{
             add(firstArgument);
             add(secondArgument);
         }};
